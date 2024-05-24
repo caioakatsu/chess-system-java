a9c6cdfe552c0a9c758817f31a2583c28b70bbc4
Caio Akatsu <kenzocaio@gmail.com>
2024-05-24 18:01:07 -0300

    Possible moves of a piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,7 +20,7 @@ public class Program {
 			UI.clearScreen();
 			UI.printBoard(chessMatch.getPieces());	
 			System.out.println();
-			System.out.println("Source: ");
+			System.out.print("Source: ");
 			ChessPosition source = UI.readChessPosition(sc);
 			
 			System.out.println();
